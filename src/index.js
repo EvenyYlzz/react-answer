63e3e3d4c7b0bf1944db4f3b8ece2ff13c5c861e
EvenyYL <172463074@qq.com>
2020-10-22 16:47:36 +0800

    配置完按需加载样式之后

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import ReactDOM from 'react-dom';
 import { Button } from 'antd-mobile';
 
 //  样式全导入
-import 'antd-mobile/dist/antd-mobile.css';
+// import 'antd-mobile/dist/antd-mobile.css';
 
 //  1.进行自动按需加载、引入样式，安装插件完成，具体查看官方文档
 //  npm install babel-plugin-import --save
 //  2.配置
-//  npm run eject 敲入命令获取所有的配置
+//  - ①npm run eject 敲入命令获取所有的配置
+//  - ②package.json 拉到最后看到babel进行配置
+//  3.配置完成后不需要再进行样式导入问题
+
 
 
 //  安装antd-mobile
